0a9aa001d6f3d73d41af8a51c1f443322ba07cc0
LeeTTrent <7557877312@GSA.GOV>
2022-12-08 11:42:06 -0600

    Updated date logic

--- SQL/Schemas/new_hrdw/nhrdw_process_log.sql
+++ SQL/Schemas/new_hrdw/nhrdw_process_log.sql
@@ -6,4 +6,4 @@ AND CREATION_DATE < '2022-12-02';
 -- nhrdw_run_snapshot SQL State: 42S22 Error Number : 1054 Error Text : Unknown column 'l_transaction_control_id' in 'field list'
 
 SELECT * FROM new_hrdw.nhrdw_process_log 
-WHERE CREATION_DATE > '2022-12-03';
+WHERE CREATION_DATE > '2022-12-05';
