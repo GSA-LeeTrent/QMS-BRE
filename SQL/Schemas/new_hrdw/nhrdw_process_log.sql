206a47d4fd803ca8b456d5c3484116a1500e2e3e
LeeTTrent <7557877312@GSA.GOV>
2022-12-15 14:55:33 -0600

    Added SQL to help troubleshoot this issue

--- SQL/Schemas/new_hrdw/nhrdw_process_log.sql
+++ SQL/Schemas/new_hrdw/nhrdw_process_log.sql
@@ -6,4 +6,6 @@ AND CREATION_DATE < '2022-12-02';
 -- nhrdw_run_snapshot SQL State: 42S22 Error Number : 1054 Error Text : Unknown column 'l_transaction_control_id' in 'field list'
 
 SELECT * FROM new_hrdw.nhrdw_process_log 
-WHERE CREATION_DATE > '2022-12-13';
+WHERE CREATION_DATE > '2022-12-15';
+
+-- Refresh QMS Staffing Error Checking SQL State: 42S22 Error Number : 1054 Error Text : Unknown column 'l_transaction_control_id' in 'field list'
