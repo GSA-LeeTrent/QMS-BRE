ac9528ef86ad25e22af5d75446196811ac219432
Lee Trent <lee.trent@gsa.gov>
2022-12-16 08:57:40 -0600

    Unassigned uss nrc (#8)
    * Working on org routing for star users
    
    * SQL/Schemas/new_hrdw/nhrdw_qms_notifications_current_v.sql
    
    * Using procedure nhrdw_star_recruit_action_status_validate that is in ACSTST
    
    * Added update statement
    
    * Added SQL file for /nhrdw_qms_notifications tbl
    
    * Added CSV files
    
    * Completed org routing change for STAR users
    
    * Working on org routing change for STAR users
    
    * Completed org routing task for STAR users
    
    * Updated date logic
    
    * Completed org routing task
    
    * Added helpful SQL to troubleshoot the USS NRC Unassigned Staffing Isssue
    
    * Added new SQL file
    
    * Added SQL to help troubleshoot this issue
    
    * Backing up USA_STAFFING rows in qms_stfacq table.
    
    * Added SQL to help troubleshoot this issue
    
    * Fixed org routing issue for USA Staffing (NRC)
    
    Co-authored-by: LeeTTrent <7557877312@GSA.GOV>

---
+++ SQL/Schemas/aca/uss_nrc_unassigned.sql
@@ -0,0 +1,17 @@
+USE aca;
+SELECT ee.qms_stfacq_error_id, ee.qms_key, ee.system_name, ee.assigned_to_org_id, oo.org_label, ee.assigned_to_user_id, uu.display_name, uu.email_address, ooo.org_label, ee.resolved_at
+FROM qms_stfacq_error ee 
+JOIN sec_org oo ON oo.org_id = ee.assigned_to_org_id
+JOIN sec_user uu ON uu.user_id = ee.assigned_to_user_id
+JOIN sec_org ooo on ooo.org_id = uu.orgId
+WHERE ee.assigned_to_user_id = 186
+AND ee.system_name = 'USA_STAFFING'
+-- AND ee.assigned_to_org_id <> 9
+-- ORDER BY ee.system_name, ee.assigned_to_org_id;
+-- ORDER BY ee.qms_stfacq_error_id ASC;
+ORDER BY ee.created_at ASC;
+
+-- SELECT distinct oo.org_label 
+-- FROM  aca.qms_stfacq_error ee
+-- JOIN sec_org oo ON oo.org_id = ee.assigned_to_org_id
+-- WHERE ee.system_name = 'USA_STAFFING'; 
